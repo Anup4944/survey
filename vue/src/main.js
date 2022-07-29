8848a12fbbbe4a8fbcba3859b9db6f3f0cb1ea9e
Anup <anuppoudel60@gmail.com>
2022-07-29 12:53:59 +1000

    basic routing setup done

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import store from "./store";
+import router from "./router";
 import "./index.css";
 
-createApp(App).use(store).mount("#app");
+createApp(App).use(store).use(router).mount("#app");
