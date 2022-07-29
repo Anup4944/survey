9ce4b9466770a0ccbfc44f78b5c33fd6f70b32b7
Anup <anuppoudel60@gmail.com>
2022-07-29 16:30:15 +1000

    dashboard, survey and routing setup done

---
+++ vue/src/components/AuthLayout.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <div
+    class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
+  >
+    <div class="max-w-md w-full space-y-8">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
